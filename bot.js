c43d3ae7c216aa068adf3d8574fc8ba2a0fb6a5e
sumit kumar <sksumitkumar003@gmail.com>
2018-03-22 03:25:38 +0530

    a bot that replies

--- bot.js
+++ bot.js
@@ -4,14 +4,32 @@ console.log('bot is running');
 var config = require('./config');
 
 var T = new Twit(config);
-tweetIt();
-setInterval(tweetIt,2000*100);
+// tweetIt();
+// setInterval(tweetIt,2000*100);
 
-function tweetIt() {
+var stream = T.stream('user');
+
+stream.on('tweet',tweetEvent);
+
+function tweetEvent(eventMsg) {
+    var replyto = eventMsg.in_reply_to_screen_name;
+    var text = eventMsg.text;
+    var from = eventMsg.user.screen_name;
+
+    console.log(replyto + ' ' + from);
+
+    if(replyto === 'sumitkumar08011'){
+        var newtweet = '@' + from + ' bot is working !!';
+        tweetIt(newtweet);
+    }
+}
+
+function tweetIt(message) {
     var r = Math.floor(Math.random()*100);
 
     var tweet = {
-        status: 'random number ' + r + ' #tweetbotworks'
+        // status: 'random number ' + r + ' #tweetbotworks'
+        status: message
     }
 
     T.post('statuses/update',tweet,tweeted);
