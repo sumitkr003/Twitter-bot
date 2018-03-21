86174244b0dc258d96568d6ab132be82de040af5
sumit kumar <sksumitkumar003@gmail.com>
2018-03-22 03:08:34 +0530

    tweeting bot

--- bot.js
+++ bot.js
@@ -3,4 +3,25 @@ console.log('bot is running');
 
 var config = require('./config');
 
-var T = new Twit(config)
+var T = new Twit(config);
+tweetIt();
+setInterval(tweetIt,2000*100);
+
+function tweetIt() {
+    var r = Math.floor(Math.random()*100);
+
+    var tweet = {
+        status: 'random number ' + r + ' #tweetbotworks'
+    }
+
+    T.post('statuses/update',tweet,tweeted);
+    
+    function tweeted(err,data,response) {
+        if(err){
+            console.log(err);
+        }else{
+            console.log('it worked !!');
+        }
+    }
+    
+}
